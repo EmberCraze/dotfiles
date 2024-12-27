a8f948b9d254e7f3e84a9cfd5a043cfbd6b59719
embercraze <maher.shaker@live.se>
2024-12-27 21:45:52 +0100

    chore: upgrade windsurf

--- files/.config/nixos/windsurf.nix
+++ files/.config/nixos/windsurf.nix
@@ -20,8 +20,8 @@ callPackage "${pkgs.path}/pkgs/applications/editors/vscode/generic.nix" rec {
   shortName = "windsurf";
 
   src = fetchurl {
-	url = "https://windsurf-stable.codeiumdata.com/linux-x64/stable/95dd76c0ef76eb2fef8d62b268ec0926f86b5eb3/Windsurf-linux-x64-1.1.1.tar.gz";
-    hash = "sha256-HMVRL1wnHPAmexBhYyfwINOhZZkdGuwW3YFXzuyW0v0=";
+	url = "https://windsurf-stable.codeiumdata.com/linux-x64/stable/599ce698a84d43160da884347f22f6b77d0c8415/Windsurf-linux-x64-1.1.2.tar.gz";
+    hash = "sha256-QAeq13K6Cnp9YF/bcElG1anchzLvGZHUgi9MWliy1u8=";
   };
 
   sourceRoot = "Windsurf";
