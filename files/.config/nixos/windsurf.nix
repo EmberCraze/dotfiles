7d10436ef9e83a82f0be4da2391dfa4fbdf19c86
embercraze <maher.shaker@live.se>
2025-03-07 13:46:39 +0100

    chore: upgrade windsurf
    We use a mix of the official windsurf package and a flake to keep up
    with the latest version available

--- files/.config/nixos/windsurf.nix
+++ files/.config/nixos/windsurf.nix
@@ -12,7 +12,7 @@
 callPackage "${pkgs.path}/pkgs/applications/editors/vscode/generic.nix" rec {
   inherit commandLineArgs useVSCodeRipgrep;
 
-  version = "1.2.6";
+  version = "1.4.4";
   pname = "windsurf";
 
   executableName = "windsurf";
@@ -20,8 +20,8 @@ callPackage "${pkgs.path}/pkgs/applications/editors/vscode/generic.nix" rec {
   shortName = "windsurf";
 
   src = fetchurl {
-	url = "https://windsurf-stable.codeiumdata.com/linux-x64/stable/d08b8ea13d580d24be204c76e5dd1651d7234cd2/Windsurf-linux-x64-1.2.6.tar.gz";
-    hash = "sha256-rXHrArkwLUzxQTwKg3Y/Rf5FXlvnTunhR3vqLoWgLKo=";
+	url = "https://windsurf-stable.codeiumdata.com/linux-x64/stable/cc9029eb32109a384927ed0ec54fcfe931879907/Windsurf-linux-x64-1.4.4.tar.gz";
+    hash = "sha256-IBWBEGmReon7bXRm9l4XohPxbVHUBekih27ADurOtr4=";
   };
 
   sourceRoot = "Windsurf";
