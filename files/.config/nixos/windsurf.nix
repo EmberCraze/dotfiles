ea7c76c1405a8d5b5d77c1a3857d52a65fb96925
embercraze <maher.shaker@live.se>
2025-03-18 10:38:11 +0100

    feat: upgrade `windsurf` to `1.4.6`

--- files/.config/nixos/windsurf.nix
+++ files/.config/nixos/windsurf.nix
@@ -12,7 +12,7 @@
 callPackage "${pkgs.path}/pkgs/applications/editors/vscode/generic.nix" rec {
   inherit commandLineArgs useVSCodeRipgrep;
 
-  version = "1.4.4";
+  version = "1.4.6";
   pname = "windsurf";
 
   executableName = "windsurf";
@@ -20,8 +20,8 @@ callPackage "${pkgs.path}/pkgs/applications/editors/vscode/generic.nix" rec {
   shortName = "windsurf";
 
   src = fetchurl {
-	url = "https://windsurf-stable.codeiumdata.com/linux-x64/stable/cc9029eb32109a384927ed0ec54fcfe931879907/Windsurf-linux-x64-1.4.4.tar.gz";
-    hash = "sha256-IBWBEGmReon7bXRm9l4XohPxbVHUBekih27ADurOtr4=";
+	url = "https://windsurf-stable.codeiumdata.com/linux-x64/stable/724a915b3b4c73cea3d2c93fc85672d6aa3961e0/Windsurf-linux-x64-1.4.6.tar.gz";
+    hash = "sha256-EW4fzv6YMhdk9Nal42qOFigrINmUw4X9Pjgm3ZlF6PQ=";
   };
 
   sourceRoot = "Windsurf";
