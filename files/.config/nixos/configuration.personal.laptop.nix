fccb6d0ec79ed3a9526c029b8fe3134a76083ade
embercraze <maher.shaker@live.se>
2026-02-12 23:57:16 +0100

    feat: add niri wm

--- files/.config/nixos/configuration.personal.laptop.nix
+++ files/.config/nixos/configuration.personal.laptop.nix
@@ -212,6 +212,9 @@ nixpkgs.config.permittedInsecurePackages = [
 	};
   };
   programs.openvpn3.enable = true;
+  programs.niri.enable = true;
+  security.pam.services.swaylock = {};
+  programs.waybar.enable = true; # top bar
 
   fonts.packages = with pkgs; [
     font-awesome
@@ -228,6 +231,7 @@ nixpkgs.config.permittedInsecurePackages = [
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
+	fuzzel swaylock mako swayidle # niri
   ];
   environment.shells = with pkgs; [ zsh ];
 
